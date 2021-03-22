dcb13e050ecce5789ad4ebdcb7f78f2aca7a1dc1
Carlos Padilla <cpadilla@soutec-group.com>
2021-03-22 03:06:51 -0400

    Cambio al script asignacion7.py

--- asignacion7.py
+++ asignacion7.py
@@ -5,4 +5,23 @@ response = requests.post(
     'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
 payload=response.json()
-pprint(payload)
+pprint(payload) 
+################### SCRIPT EDITADO #############################
+data = payload.get('Token')
+response = requests.get(
+'https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device',
+headers={'X-Auth-Token':data}
+)
+################################################
+x=response.text.encode('utf8')
+nameList = []
+nameList = json.loads(x)
+################### TABLA DE DATOS #############################
+for response in nameList['response']:
+  print('family:', response['family'])
+  print('hostname:', response['hostname'])
+  print('managementIpAddress:', response['managementIpAddress'])
+  print('lastUpdated:', response['lastUpdated'])
+  print('reachabilityStatus:', response['reachabilityStatus'])
+  print('')
+
